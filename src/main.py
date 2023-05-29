82f59152eb744c36f8b22ec43fa5ddf1882988ca
Arthur Riechert <riechertarthur@gmail.com>
2023-05-29 02:45:52 -0400

    Put multiple input model into main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import models.single_input_linear_regression as silc
+import models.multiple_input_linear_regression as milc
 
 # Run test for our model.
 if __name__ == "__main__":
-    silc.test()
+    # silc.test()
+    milc.test_1()
